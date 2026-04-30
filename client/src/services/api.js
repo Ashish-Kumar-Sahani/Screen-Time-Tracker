506b4e81ba9330b9ef1b51d04916219cfd2fb55f
Ashish Kumar <ak6940390@gmail.com>
2026-05-01 00:08:10 +0530

    fix cors final

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://screen-time-tracker-1.onrender.com/api",
+  baseURL: "https://screen-time-tracker-vr7o.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
